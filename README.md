13ff978569927c8752949b20e0426c088d6a0eb7
Mr <jpihlk@yahoo.com>
2023-08-17 21:06:53 -0400

    added async with examples

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # top-npm-packages
 combine the 15 most popular npm packages into one app
+based on the article [here](https://www.turing.com/blog/top-npm-packages-for-node-js-developers/)
+
+
+
+### Whats been done with what: 
+1 - Express : node framework. set up basic server - one demo endpoint for future use
+
+2 - async: - I haven't used this so im watching this video [Here](https://www.youtube.com/watch?v=W5x8v8XEGx8) and coding along. Post Video: Awesome library for managing multiple Promises. 
--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const async = require('async')
+
+
+//sends in parallel - returns array of values
+//// Reflect makes values return as objects- otherwise returns as array but on error only returns error. Returns in order they came in.
+async.parallel([
+  function(callback) {
+    setTimeout(function() {
+        callback(null, 'two');
+    }, 5000)},
+  async.reflect(async () => await Promise.resolve('Like')),
+  function(callback) {
+    setTimeout(function() {
+        callback(null, 'two again');
+    }, 1000)},
+  async.reflect(async () => await Promise.resolve('Share')),
+])
+.then(response =>{
+  console.log('response', response);
+})
+.catch(err => {
+  console.log(err)
+})
+
+// also async.forEachOfLimit(arrayToItterate, numOfAsyncOperationsAtOnce, [value,index, callback])
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
