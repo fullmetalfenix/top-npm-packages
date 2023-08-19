28c335745e47d3f791cff0429a7c9aaee741a54c
Mr <jpihlk@yahoo.com>
2023-08-18 22:41:14 -0400

    added dotenv for cloudinary

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ combine the 15 most popular npm packages into one app
 based on the article [here](https://www.turing.com/blog/top-npm-packages-for-node-js-developers/)
 
 *** Added nodemon as well as I got tired of restarting express
+**** Also added dotenv for cloudinary keys
 
 ### Whats been done with what: 
 1 - Express : node framework. set up basic server - one demo endpoint for future use
@@ -10,3 +11,5 @@ based on the article [here](https://www.turing.com/blog/top-npm-packages-for-nod
 2 - async: - I haven't used this so im watching this video [Here](https://www.youtube.com/watch?v=W5x8v8XEGx8) and coding along. Post Video: Awesome library for managing multiple Promises.  
 
 3 - Lodash: utility library that is usefull for working with data / objects - example is with filter for all objects but could do find for first etc. [here](https://lodash.com/docs/4.17.15)
+
+4 - Cloudinary: 
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = 3000;
 const async = require("async");
 const _ = require("lodash");
+// const cloudinary = require('cloudinary').v2
+// cloudinary.url("sample.jpg", {width: 100, height: 150, crop: "fill", fetch_format: "auto"})
+
+
+
 //sends in parallel - returns array of values
 //// Reflect makes values return as objects- otherwise returns as array but on error only returns error. Returns in order they came in.
 async
@@ -42,6 +48,14 @@ app.get("/", (req, res) => {
   var getUsers = _.filter(posts, { town: "New York" });
   res.send(getUsers);
 });
+
+app.get('/test', (req,res) =>{
+  res.send(process.env.USER_ID)
+})
+
+// app.get("/cloudinary", (req, res) =>{
+//   res.send(cloudinary.image("https://res.cloudinary.com/demo/image/upload/c_crop,h_200,w_300,x_355,y_410/c_fill,h_100,w_130/brown_sheep.jpg", { width: 500, height: 500, crop: "fill", version: "1573726751", cloud_name: "demo", secure: "true", alt: "Casual Jacket"}))
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
